249416761b921d2279a4523104ea56046badfee8
Hiago <hiago.lst@gmail.com>
2022-03-31 20:34:21 -0300

    teste

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-
   getHello(): string {
     return 'Hello World!';
   }
 
   getIdade(ano: number) {
-    
     const anoAtual = new Date().getFullYear();
     const idade = anoAtual - ano;
-
+    console.log('Teste');
     return 'A idade do individuo é ' + idade + ' anos';
   }
-
-
 }
